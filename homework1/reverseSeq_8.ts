6e5a6b965ad4ca1fb426651f6d0c21eb07060577
daschle <dash.q.n@gmail.com>
2026-09-16 20:33:53 -0700

    Fixed old stuff

--- homework1/reverseSeq_8.ts
+++
@@ -1,7 +0,0 @@
-//https://www.codewars.com/kata/5a00e05cc374cb34d100000d
-
-const reverseSeq = (n: number): number[] => {
-  let res: number[] = [];
-  for(let i = n; i > 0; i--) res.push(i);
-  return res;
-};
--- project4/tssrc/games/game1.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Component, ReactNode } from "react";
-
-export class Game1 extends Component {
-    override render(): ReactNode {
-        return <div>
-            <p id = "Game1">Game1</p>
-        </div>
-    }
-}
